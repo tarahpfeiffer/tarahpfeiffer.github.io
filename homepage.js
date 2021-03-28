f29e4c989ccd49157d0382ad518f9dbc51a232f0
tarahpfeiffer <81434509+tarahpfeiffer@users.noreply.github.com>
2021-03-28 18:46:07 +0200

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -5,8 +5,10 @@ const nav = document.getElementById("navbar");
 
 openNavbar.addEventListener("click", function() {
     nav.style.visibility="unset";
+    openNavbar.style.visibility="hidden";
 })
 
 closeNavbar.addEventListener("click", function() {
     nav.style.visibility="hidden";
+    openNavbar.style.visibility="unset";
 })
